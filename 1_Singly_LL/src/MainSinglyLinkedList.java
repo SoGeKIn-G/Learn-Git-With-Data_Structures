cc18568991a473e356e23480862b158e0e245e8a
Gaurav Singh Bora <gauravbora263642@gmail.com>
2022-12-08 20:26:02 +0530

    initial commit

---
+++ 1_Singly_LL/src/MainSinglyLinkedList.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList sLL = new SinglyLinkedList();
+        sLL.createSinglyLinkedList(5);
+        // System.out.println(sLL.head.value);
+        sLL.insertInLinkedList(6, 1);
+        sLL.insertInLinkedList(7, 3);
+        sLL.insertInLinkedList(8, 4);
+        sLL.insertInLinkedList(9, 0);
+        sLL.traverseSinglyLinkedList();
+        sLL.deleteSLL();
+        sLL.traverseSinglyLinkedList();
+
+    }
+}
